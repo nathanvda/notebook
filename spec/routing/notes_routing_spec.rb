6c87a4e439a7ed29b6beaf5b6515714f6dc7d8f4
nathanvda <nathan@dixis.com>
2011-11-08 15:04:10 +0100

    Fix the routing specs.

--- spec/routing/notes_routing_spec.rb
+++ spec/routing/notes_routing_spec.rb
@@ -3,28 +3,16 @@ require "spec_helper"
 describe NotesController do
   describe "routing" do
 
-    it "routes to #index" do
-      get("/notes").should route_to("notes#index")
-    end
-
-    it "routes to #new" do
-      get("/notes/new").should route_to("notes#new")
-    end
-
-    it "routes to #show" do
-      get("/notes/1").should route_to("notes#show", :id => "1")
-    end
-
-    it "routes to #edit" do
-      get("/notes/1/edit").should route_to("notes#edit", :id => "1")
+    it "routes to #content" do
+      get("/notes/content").should route_to("notes#content")
     end
 
     it "routes to #create" do
-      post("/notes").should route_to("notes#create")
+      post("/books/1/notes").should route_to("notes#create", :book_id => "1")
     end
 
     it "routes to #update" do
-      put("/notes/1").should route_to("notes#update", :id => "1")
+      put("/notes/update").should route_to("notes#update")
     end
 
     it "routes to #destroy" do
